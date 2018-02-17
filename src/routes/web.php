f8bd39d24ab89ab8907d14d2cfc19e32bd9a0809
zono <osato@mbp.local>
2018-02-17 16:35:02 +0900

    タスクの作成、保存処理を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,14 +10,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('tasks');
 });
 
 Route::post('/task',function (Request $request) {
+  $validator = Validator::make($request->all(),[
+    'name'=>'required|max:225',
+  ]);
 
+  if($validator->fails()) {
+    return redirect('/')
+    ->withInput()
+    ->withErrors($validator);
+  }
+
+  $task = new Task;
+  $task->name = $request->name;
+  $task->save();
+
+  return redirect('/');
 });
+
+
 Route::delete('/task/{task}',function (Task $task) {
 
 });
